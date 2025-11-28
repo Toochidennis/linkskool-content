2b7584146b341bb146f542bd27c7e982fac26777
ToochiDennis <dennistoochukwu@gmail.com>
2025-11-28 23:03:45 +0100

    api services

--- src/api/methods/post.ts
+++
@@ -1,8 +0,0 @@
-import { client } from '../client';
-import { request } from '../request';
-
-export const apiPost = <T, D>(endpoint: string, data: D) =>
-  request<T>(() => client.post(endpoint, data));
-
-export const create = <T, D>(endpoint: string, payload: D) =>
-  apiPost<T, D>(endpoint, payload);
